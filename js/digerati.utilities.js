a9b3e1bd26b03b41180e08f7095aa1578215199e
Digerati <127671144+CabalOfDigerati@users.noreply.github.com>
2023-05-03 09:45:51 +0100

    Update digerati.utilities.js

--- js/digerati.utilities.js
+++ js/digerati.utilities.js
@@ -108,6 +108,7 @@ class DigeratiUtilities {
         this.displaySearchTermInResultsPage();
         this.focusSearchFormField();
         this.skipToMainContent();
+        this.toggleSearchDisplay();
     }
 
     /**
