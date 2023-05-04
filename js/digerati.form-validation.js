6744e4af24c140083906cb13c77e1c84dac0623a
Digerati <127671144+CabalOfDigerati@users.noreply.github.com>
2023-05-04 09:25:58 +0100

    Update digerati.form-validation.js
    Ensure submit button exists before adding event listeners - due to Webflow piss poor handling of form properties ATM

--- js/digerati.form-validation.js
+++ js/digerati.form-validation.js
@@ -327,22 +327,24 @@ class DigeratiFormValidation {
             if(!form.getAttribute('novalidate')) {
                 /* Submit Button Event Listeners */
                 const submitButton = form.querySelector('input[type=submit]');
-                submitButton.addEventListener('click', this.handleSubmitEvent);
-                submitButton.addEventListener('touchstart', this.handleSubmitEvent);
-                /* Input and Textarea Field Event Listeners */
-                const inputAndTextareaFields = form.querySelectorAll('input:not([type="submit"]), textarea');
-                inputAndTextareaFields.forEach((formField) => {
-                    formField.addEventListener('focus', () => {
-                        formField.removeEventListener('blur', this.triggerFormFieldValidation);
-                        formField.removeEventListener('keyup', this.triggerFormFieldValidation);
-                        formField.addEventListener('blur', () => {
-                            this.triggerFormFieldValidation(formField);
-                        });
-                        formField.addEventListener('keyup', () => {
-                            this.triggerFormFieldValidation(formField)
+                if(!submitButton) {
+                    submitButton.addEventListener('click', this.handleSubmitEvent);
+                    submitButton.addEventListener('touchstart', this.handleSubmitEvent);
+                    /* Input and Textarea Field Event Listeners */
+                    const inputAndTextareaFields = form.querySelectorAll('input:not([type="submit"]), textarea');
+                    inputAndTextareaFields.forEach((formField) => {
+                        formField.addEventListener('focus', () => {
+                            formField.removeEventListener('blur', this.triggerFormFieldValidation);
+                            formField.removeEventListener('keyup', this.triggerFormFieldValidation);
+                            formField.addEventListener('blur', () => {
+                                this.triggerFormFieldValidation(formField);
+                            });
+                            formField.addEventListener('keyup', () => {
+                                this.triggerFormFieldValidation(formField)
+                            });
                         });
                     });
-                });
+                }
                 /* Finsweet Custom Select Event Listeners */
                 const selectFields = form.querySelectorAll('select');
                 selectFields.forEach((formField) => {
