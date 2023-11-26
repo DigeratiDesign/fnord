bf3e2415fb9ce06d34da81e3f9c38b1bf7904046
Digerati <127671144+CabalOfDigerati@users.noreply.github.com>
2023-11-26 19:05:23 +0000

    Update digerati.form-validation.js

--- js/digerati.form-validation.js
+++ js/digerati.form-validation.js
@@ -56,8 +56,8 @@ class DigeratiFormValidation {
     getErrorElements(formField, formFieldType) {
         let errorElements = {};
         if (formFieldType !== 'select') {
-            if(formFieldType === 'textarea' && formField.id == 'g-recaptcha-response') {
-                console.log('ReCaptcha textarea');
+            if(formFieldType === 'textarea' && formField.id == 'h-captcha-response') {
+                console.log('hCaptcha textarea');
                 /* ReCaptcha */
                 errorElements.border = formField.parentElement.parentElement;
                 errorElements.message = errorElements.border.nextElementSibling;
